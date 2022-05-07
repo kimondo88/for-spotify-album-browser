3dad7d1788d63e7d8c8558fd0b451e120fbc1781
kimondo88 <kimondo1988@o2.pl>
2022-05-07 11:09:25 +0200

    making login component

---
+++ client/src/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=a481dc4ec02b423f8899ad1d33295e34&
+response_type=code&redirect_uri=http://localhost:3000&state=&scope=streaming%20user-read-email%20user-read-private
+%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+
+
+export default function Login(){
+    return (
+        <div>
+
+        </div>
+    )
+}
